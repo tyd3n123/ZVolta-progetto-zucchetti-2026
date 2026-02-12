aca28a1f5ac0682218f05bb85738757795276658
darius <tyd3ndesign@gmail.com>
2026-02-12 11:46:20 +0100

    commit bello

---
+++ public/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <title>Login | Z Volta</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./login.css">
+</head>
+<body >
+    <div class="login-container">
+      <h1>Z VOLTA</h1>
+      <form method="POST" action="">
+        <div class="field">
+          <label for="email">ID utente</label>
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            placeholder="es. cAr5tAmA77Ia">
+        </div>
+
+        <div class="field password-field">
+          <label for="password">Password</label>
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            placeholder="************">
+        </div>
+
+        <button type="submit" class="login-btn">
+          Login
+        </button>
+      </form>
+    </div>
+</body>
+</html>
